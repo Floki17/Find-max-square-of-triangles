84c855b056d83dcdf13d5f67186138ad3c01317f
Floki17 <101497123+Floki17@users.noreply.github.com>
2022-03-13 12:39:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,67 +1,16 @@
-import telebot
-from telebot import types
-import datetime
-import wikipedia, re
-token = "5175610928:AAFRiVFrQMciTMV5-lCDfIihDaVOA6k0uB4"
-bot = telebot.TeleBot(token)
-wikipedia.set_lang("ru")
+sides = [3, 2, 4, 7, 5, 12, 11, 13, 15, 16, 14, 14]
+sides = sorted(sides, reverse=True)
+smax = 0
+for i in range(len(sides)):
+    for j in range(i + 1, len(sides)):
+        for k in range(j + 1, len(sides)):
+            a = sides[i]
+            b = sides[j]
+            c = sides[k]
+            if a + b > c and a + c > b and b + c > a:
+                p = (a + b + c) / 2
+                s = (p * (p - a) * (p - b) * (p - c)) ** (1 / 2)
+                if s > smax:
+                    smax = s
 
-
-def wik(s):
-    try:
-        ny = wikipedia.page(s)
-        wikitext = ny.content[:1000]
-        wikimas = wikitext.split('.')
-        wikimas = wikimas[:-1]
-        wikitext2 = ''
-        for x in wikimas:
-            if not ('==' in x):
-                if (len((x.strip())) > 3):
-                    wikitext2 = wikitext2 + x + '.'
-            else:
-                break
-        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2 = re.sub('\([^()]*\)', '', wikitext2)
-        wikitext2 = re.sub('\{[^\{\}]*\}', '', wikitext2)
-        return wikitext2
-    except Exception as e:
-            return 'В энциклопедии нет информации об этом'
-
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    keyboard = types.ReplyKeyboardMarkup()
-    keyboard.row("/Date", "/Time", "/News", "/help",)
-    bot.send_message(message.chat.id, "Отправьте мне любое слово, и я найду его значение на Wikipedia", reply_markup=keyboard)
-
-
-@bot.message_handler(commands=["help"])
-def start_message(message):
-    bot.send_message(message.chat.id, "Я могу найти любую информацию в Wikipedia которая вас может заинтересовать. \n\nВозможные команды:\n/Time - Для вывода точного времени \n/Date - для вывода точной даты \n/News - для вывода новостей " )
-
-
-@bot.message_handler(commands=["Date"])
-def date_message(message):
-    Date = datetime.datetime.now()
-    dt = Date.strftime("Дата: %d/%m/%Y")
-    bot.send_message(message.chat.id, dt)
-
-
-@bot.message_handler(commands=["Time"])
-def time_message(message):
-    Date = datetime.datetime.now()
-    vr = Date.strftime("Время: %H:%M:%S")
-    bot.send_message(message.chat.id, vr)
-
-
-@bot.message_handler(commands=["News"])
-def news_message(message):
-    bot.send_message(message.chat.id, 'https://tass.ru')
-
-
-@bot.message_handler(content_types=["text"])
-def wik2(message):
-    bot.send_message(message.chat.id, wik(message.text))
-
-
-bot.polling(none_stop=True, interval=0)
+print("Максимальная площадь треугольника", smax)
